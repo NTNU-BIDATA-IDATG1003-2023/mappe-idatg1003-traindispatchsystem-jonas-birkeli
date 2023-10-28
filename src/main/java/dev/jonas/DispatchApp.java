af7131d71ff35321d9647a5d4993455319091a45
Jonas <jonas.birkeli@hotmail.no>
2023-10-28 17:28:55 +0200

    Functional program implementation

--- src/main/java/dev/jonas/DispatchApp.java
+++ src/main/java/dev/jonas/DispatchApp.java
@@ -67,6 +67,14 @@ public class DispatchApp {
     currentTime = new int[]{0, 0};
     scanner = new Scanner(System.in);
 
+    TrainDeparture dep1 = new TrainDeparture(new int[]{23, 56}, "L1", "Hamburg", 2, 1);
+    TrainDeparture dep2 = new TrainDeparture(new int[]{23, 57}, "L2", "Hamburg", 4, 2);
+    TrainDeparture dep3 = new TrainDeparture(new int[]{23, 58}, "L3", "Hamburg", 1, 3);
+
+    departures.add(dep1);
+    departures.add(dep2);
+    departures.add(dep3);
+
     // Starts program to run continuously until user chooses to exit
     // Uses a switch statement to determine which method to run,
     // based on the current state of the program
@@ -179,31 +187,158 @@ public class DispatchApp {
     // TODO: Implement method to exit when user presses enter
   }
 
+  /**
+   * Adds a {@code TrainDeparture} to the list of departures.
+   * If some info is not parsable to correct data types,
+   * the user is asked to try again.
+   * {@code TrainDeparture} is added to the list of departures when all info is valid.
+   *
+   * @since 1.0.0
+   * @see TrainDeparture
+   */
   private void addTrainDeparture() {
-    // TODO: Implement method
+    String[] fields = {"Departure hour", "Departure minute", "Line", "Destination", "Track", "Train number"};
+    String[] values = new String[fields.length];
+    boolean validInput = false;
+
+    while (!validInput) {
+      // Continuously ask for user input until a valid choice is made
+      // Stores user input in values array
+      for (int i = 0; i < fields.length; i++) {
+        System.out.println(fields[i] + ": ");
+        values[i] = scanner.nextLine();
+      }
+      try {
+        // Try to parse user input to correct data types
+        int[] departureTime = new int[]{Integer.parseInt(values[0]), Integer.parseInt(values[1])};
+        String line = values[2];
+        String destination = values[3];
+        int track = Integer.parseInt(values[4]);
+        int trainNumber = Integer.parseInt(values[5]);
+        TrainDeparture trainDeparture = new TrainDeparture(departureTime, line, destination, track, trainNumber);
+        departures.add(trainDeparture);
+
+        // If no exception is thrown, the input is valid.
+        validInput = true;
+      } catch (NumberFormatException | NoSuchElementException | IllegalStateException e) {
+        System.out.println("Invalid input. Please try again.");
+      }
+    }
+    // Change state of program to main menu
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Assigns a track to a {@code TrainDeparture}.
+   * If no {@code TrainDeparture} is selected, the method returns early.
+   *
+   * @since 1.0.0
+   */
   private void assignTrackToTrainDeparture() {
-    // TODO: Implement method
+    if (selectedDeparture == null) {
+      System.out.println("No train departure selected. Please try again.");
+      this.state = STATE_MAIN_MENU;
+      return;
+    }
+
+    System.out.println("Assign track to train departure");
+    System.out.println(selectedDeparture.getDetails());
+    System.out.println("Enter track number: ");
+    int track = Integer.parseInt(scanner.nextLine());
+    selectedDeparture.setTrack(track);
+
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Assigns a delay to a {@code TrainDeparture}.
+   * If no {@code TrainDeparture} is selected, the method returns early.
+   *
+   * @since 1.0.0
+   */
   private void assignDelayToTrainDeparture() {
-    // TODO: Implement method
+    if (this.selectedDeparture == null) {
+      System.out.println("No train departure selected. Please try again.");
+      this.state = STATE_MAIN_MENU;
+      return;
+    }
+
+    System.out.println("Assign delay to train departure");
+    System.out.println(this.selectedDeparture.getDetails());
+    System.out.println("Enter delay in the format {HH, mm}: ");
+    String[] delay = scanner.nextLine().split(", ");
+    this.selectedDeparture.setDelay(new int[]{Integer.parseInt(delay[0]), Integer.parseInt(delay[1])});
+
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Searches for a {@code TrainDeparture} by train number.
+   * If no {@code TrainDeparture} is found, the method returns early.
+   *
+   * @since 1.0.0
+   */
   private void searchTrainDepartureByNumber() {
-    // TODO: Implement method
+    System.out.println("Search train departure by number");
+    System.out.println("Enter train number: ");
+    int trainNumber = Integer.parseInt(scanner.nextLine());
+    for (TrainDeparture departure : this.departures) {  // TODO: Implement streams
+      if (departure.getTrainNumber() == trainNumber) {
+        this.selectedDeparture = departure;
+        System.out.println("Train departure found:");
+        System.out.println(selectedDeparture.getDetails());
+        this.state = STATE_MAIN_MENU;
+        return;
+      }
+    }
+    System.out.println("No train departure found with train number " + trainNumber + ". Please try again.");
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Searches for a {@code TrainDeparture} by destination.
+   * If no {@code TrainDeparture} is found, the method returns early.
+   *
+   * @since 1.0.0
+   */
   private void searchTrainDepartureByDestination() {
-    // TODO: Implement method
+    System.out.println("Search train departure by destination");
+    System.out.println("Enter destination: ");
+    String destination = scanner.nextLine();
+    for (TrainDeparture departure : this.departures) {  // TODO: Implement streams
+      if (departure.getDestination().equals(destination)) {
+        this.selectedDeparture = departure;  // Update selected departure
+        System.out.println("Train departure found:");
+        System.out.println(selectedDeparture.getDetails());
+        this.state = STATE_MAIN_MENU;
+        return;
+      }
+    }
+    System.out.println("No train departure found with destination " + destination + ". Please try again.");
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Changes the time of the program.
+   *
+   * @since 1.0.0
+   */
   private void changeTime() {
-    // TODO: Implement method
+    System.out.println("Change time");
+    System.out.println("Enter new time in the format {HH, mm}: ");
+    String[] time = scanner.nextLine().split(", ");
+    this.currentTime = new int[]{Integer.parseInt(time[0]), Integer.parseInt(time[1])};
+    this.state = STATE_MAIN_MENU;
   }
 
+  /**
+   * Exits the application.
+   *
+   * @since 1.0.0
+   */
   private void exitApplication() {
-    // TODO: Implement method
+    System.out.println("Exiting application...");
+    this.state = STATE_MAIN_MENU;
+    this.running = false;
   }
 }
