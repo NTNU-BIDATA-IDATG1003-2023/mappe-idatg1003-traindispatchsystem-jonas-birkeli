1ad8904adc985d31a6fa0939d46858d8f0ce149b
Jonas <jonas.birkeli@hotmail.no>
2023-10-28 12:55:31 +0200

    Method implementation, docstrings and constructor done

--- src/main/java/dev/jonas/TrainDeparture.java
+++ src/main/java/dev/jonas/TrainDeparture.java
@@ -1,5 +1,13 @@
 package dev.jonas;
 
+/**
+ * The {@code TrainDeparture} class represents a train on a station.
+ * All {@code TrainDeparture}s has a departuretime, delay, line, destination and track.
+ *
+ * @author Jonas Birkeli
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class TrainDeparture {
 
   private int[] departureTime;
@@ -9,66 +17,200 @@ public class TrainDeparture {
   private int track;
   private int trainNumber;
 
+  /**
+   * Constructs a new {@code TrainDeparture} with default values.
+   *
+   * @since 1.0.0
+   */
   public TrainDeparture() {
-    // TODO imp constructor
+    setDepartureTime(new int[]{0, 0});
+    setLine("");
+    setDestination("");
+    setTrack(-1);
+    setDelay(new int[]{0, 0});
   }
 
+  /**
+   * Sets the departure time of the {@code TrainDeparture}.
+   * Each {@code TrainDeparture} has two departure times,
+   * one representing hours, and one representing minutes.
+   * If null, departure time is set to 0.
+   *
+   * @param departureTime the departure time of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public void setDepartureTime(int[] departureTime) {
-    // TODO imp setDepartureTime
+    if (departureTime == null) {
+      this.departureTime = new int[]{0, 0};
+      return;
+    }
+    this.departureTime = departureTime;
   }
 
+  /**
+   * Returns the departure time of the {@code TrainDeparture}.
+   *
+   * @return the departure time of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public int[] getDepartureTime() {
-    // TODO imp getDepartureTime
-    return null;
+    return departureTime;
   }
 
+  /**
+   * Sets the delay of the {@code TrainDeparture}.
+   * Each {@code TrainDeparture} has two delays,
+   * one representing hours, and one representing minutes.
+   * If null, delay is set to 0.
+   *
+   * @param delay the delay of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public void setDelay(int[] delay) {
-    // TODO imp setDelay
+    if (delay == null) {
+      this.delay = new int[]{0, 0};
+      return;
+    }
+    this.delay = delay;
   }
 
+  /**
+   * Returns the delay of the {@code TrainDeparture}.
+   *
+   * @return the delay of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public int[] getDelay() {
-    // TODO imp getDelay
-    return null;
+    return delay;
   }
 
+  /**
+   * Sets the line of the {@code TrainDeparture}.
+   * If null, line is set to empty.
+   *
+   * @param line the line of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public void setLine(String line) {
-    // TODO imp setLine
+    if (line == null) {
+      this.line = "";
+      return;
+    }
+    this.line = line;
   }
 
+  /**
+   * Returns the line of the {@code TrainDeparture}.
+   *
+   * @return the line of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public String getLine() {
-    // TODO imp getLine
-    return null;
+    return line;
   }
 
+  /**
+   * Sets the destination of the {@code TrainDeparture}.
+   * If null, destination is set to empty.
+   *
+   * @param destination the destination of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public void setDestination(String destination) {
-    // TODO imp setDestination
+    if (destination == null) {
+      this.destination = "";
+      return;
+    }
+    this.destination = destination;
   }
 
+  /**
+   * Returns the destination of the {@code TrainDeparture}.
+   *
+   * @return the destination of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public String getDestination() {
-    // TODO imp getDestination
-    return null;
+    return destination;
   }
 
+  /**
+   * Sets the track of the {@code TrainDeparture}.
+   * If negative, track is set to -1.
+   *
+   * @param track the track of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public void setTrack(int track) {
-    // TODO imp setTrack
+    if (track < 0) {
+      this.track = -1;
+      return;
+    }
+    this.track = track;
   }
 
+  /**
+   * Returns the track of the {@code TrainDeparture}.
+   *
+   * @return the track of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public int getTrack() {
-    // TODO imp getTrack
-    return -1;
+    return track;
   }
 
+  /**
+   * Sets a train number for the {@code TrainDeparture}.
+   * If negative, train number is set to -1.
+   * Number must be unique for each {@code TrainDeparture}.
+   *
+   * @since 1.0.0
+   */
   public void setTrainNumber(int trainNumber) {
-    // TODO imp setTrainNumber
+    if (trainNumber < 0) {
+      this.trainNumber = -1;
+      return;
+    }
+    this.trainNumber = trainNumber;
   }
 
+  /**
+   * Returns the train number of the {@code TrainDeparture}.
+   *
+   * @return the train number of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public int getTrainNumber() {
-    // TODO imp getTrainNumber
-    return -1;
+    return trainNumber;
   }
 
+  /**
+   * Returns a string representation of the {@code TrainDeparture}.
+   * Similar to what you see at an actual train station.
+   *
+   * @see #getLine()
+   * @see #getTrack()
+   * @see #getDepartureTime()
+   * @see #getDestination()
+   * @return a string representation of the {@code TrainDeparture}.
+   * @since 1.0.0
+   */
   public String getDetails() {
-    // TODO imp getDetails
-    return null;
+    StringBuilder objectInfomation;
+    objectInfomation = new StringBuilder();
+
+
+    objectInfomation
+        .append(getLine())
+        .append(" ")
+        .append(getTrack())
+        .append(" ")
+        .append(getDepartureTime()[0])
+        .append(":")
+        .append(getDepartureTime()[1])
+        .append(" ")
+        .append(getDestination());
+
+    return objectInfomation.toString();
   }
 }
