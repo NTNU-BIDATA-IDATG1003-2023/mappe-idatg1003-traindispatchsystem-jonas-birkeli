584ed905cd2aa096efed27a3cc3b609826423790
Jonas <jonas.birkeli@hotmail.no>
2023-10-28 13:08:44 +0200

    Dispatch App created with temporary filler methods

---
+++ src/main/java/dev/jonas/DispatchApp.java
@@ -0,0 +1,53 @@
+package dev.jonas;
+
+/**
+ * The {@code DispatchApp} class represents the main class of the program.
+ * The {@code DispatchApp} class handles all user input and output,
+ * and runs continuously until the user chooses to exit the program.
+ *
+ * @author Jonas Birkeli
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class DispatchApp {
+  public static void main(String[] args) {
+   DispatchApp dispatchApp = new DispatchApp();
+   dispatchApp.start();
+  }
+
+  public void start() {
+    // TODO: Implement loop for program
+  }
+
+  private void viewTrainDepartures() {
+    // TODO: Implement method
+  }
+
+  private void addTrainDeparture() {
+    // TODO: Implement method
+  }
+
+  private void assignTrackToTrainDeparture() {
+    // TODO: Implement method
+  }
+
+  private void assignDelayToTrainDeparture() {
+    // TODO: Implement method
+  }
+
+  private void searchTrainDepartureByNumber() {
+    // TODO: Implement method
+  }
+
+  private void searchTrainDepartureByDestination() {
+    // TODO: Implement method
+  }
+
+  private void changeTime() {
+    // TODO: Implement method
+  }
+
+  private void exitApplication() {
+    // TODO: Implement method
+  }
+}
