f87f36fc563626a08c6bffb11ef04aa52f37cce9
Jonas <jonas.birkeli@hotmail.no>
2023-10-28 12:39:29 +0200

    Structural commit

---
+++ src/main/java/dev/jonas/TrainDeparture.java
@@ -0,0 +1,74 @@
+package dev.jonas;
+
+public class TrainDeparture {
+
+  private int[] departureTime;
+  private int[] delay;
+  private String line;
+  private String destination;
+  private int track;
+  private int trainNumber;
+
+  public TrainDeparture() {
+    // TODO imp constructor
+  }
+
+  public void setDepartureTime(int[] departureTime) {
+    // TODO imp setDepartureTime
+  }
+
+  public int[] getDepartureTime() {
+    // TODO imp getDepartureTime
+    return null;
+  }
+
+  public void setDelay(int[] delay) {
+    // TODO imp setDelay
+  }
+
+  public int[] getDelay() {
+    // TODO imp getDelay
+    return null;
+  }
+
+  public void setLine(String line) {
+    // TODO imp setLine
+  }
+
+  public String getLine() {
+    // TODO imp getLine
+    return null;
+  }
+
+  public void setDestination(String destination) {
+    // TODO imp setDestination
+  }
+
+  public String getDestination() {
+    // TODO imp getDestination
+    return null;
+  }
+
+  public void setTrack(int track) {
+    // TODO imp setTrack
+  }
+
+  public int getTrack() {
+    // TODO imp getTrack
+    return -1;
+  }
+
+  public void setTrainNumber(int trainNumber) {
+    // TODO imp setTrainNumber
+  }
+
+  public int getTrainNumber() {
+    // TODO imp getTrainNumber
+    return -1;
+  }
+
+  public String getDetails() {
+    // TODO imp getDetails
+    return null;
+  }
+}
